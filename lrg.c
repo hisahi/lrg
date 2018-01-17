318de36f6e39566df2298a8c7f98b27a5546ee1a
Sampo Hippeläinen <sampo.hippelainen@gmail.com>
2018-01-17 11:15:49 +0200

    update year in lrg.c license

--- lrg.c
+++ lrg.c
@@ -5,7 +5,7 @@ A tool that allows displaying specific lines
 of files (or around a specific line)
 with possible line number display
 
-Copyright (c) 2017 Sampo Hippeläinen (hisahi)
+Copyright (c) 2017-2018 Sampo Hippeläinen (hisahi)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
